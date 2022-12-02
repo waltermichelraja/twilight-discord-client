c3d3e26d2d583fe68830bf615f5d79893a718143
Walter Raja <waltermichelraja2@gmail.com>
2022-12-03 00:15:05 +0530

    updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 import os
 from discord import app_commands
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 intents=discord.Intents.all()
@@ -9,16 +9,22 @@ client=commands.Bot(command_prefix=";", intents=intents)
 load_dotenv()
 TOKEN=os.getenv("TOKEN")
 
+status=(["SONGS", ";HELP"])
 @client.event
 async def on_ready():
+    change_status.start()
     print("--Twilight\'s online!--")
     try:
         sync=await client.tree.sync()
-        print(f"commands synced: {len(sync)}")
+        print(f"--commands synced: {len(sync)}--")
     except Exception as e:
         print(e)
 
-@client.tree.command(name="ping")
+@tasks.loop(minutes=5)
+async def change_status():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"{status}"))
+
+@client.tree.command(name="ping", description="sends client latency")
 async def ping(interaction:discord.Interaction):
     await interaction.response.send_message(f"```Pong! \nLATENCY: {client.latency*1000:,.0f} ms```", ephemeral=True)
 
