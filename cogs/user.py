80ac6d56658a9095393b7ee6d3379ad88eea6317
Walter Raja <waltermichelraja2@gmail.com>
2022-12-03 22:24:21 +0530

    updated

--- cogs/user.py
+++ cogs/user.py
@@ -6,9 +6,6 @@ class User(commands.Cog):
     def __init__(self, client: commands.Client) -> None:
         self.client = client
 
-@app_commands.guild_only()
-class Group(app_commands.Group):
-
     @app_commands.command(name="cog", description="cog test")
     async def cog(self, interaction:discord.Interaction) -> None:
         await interaction.response.send_message("hello!", ephemeral=True)
