b3e46b44f62fa0550d1d4ab1dd1b0391cf33cadb
Walter Raja <waltermichelraja2@gmail.com>
2022-12-04 01:26:04 +0530

    updated

--- cogs/user.py
+++ cogs/user.py
@@ -9,7 +9,7 @@ class User(commands.Cog):
     @app_commands.command(name="userinfo", description="sends user-details")
     async def userinfo(self, interaction:discord.Interaction, user:discord.User=None):
         user=interaction.user if not user else user
-        embed=discord.Embed(title="User Details", color = interaction.user.color, timestamp = interaction.created_at)
+        embed=discord.Embed(title=f"User Details | {interaction.user}", color = interaction.user.color, timestamp = interaction.created_at)
         embed.add_field(name="Account creation: ", value=user.created_at.strftime("```%a, %#d %B %Y, %I:%M %p```"), inline=True)
         embed.add_field(name="Joined at: ", value=user.joined_at.strftime("```%a, %#d %B %Y, %I:%M %p```"), inline=True)
         embed.set_footer(text = f"Requested by {interaction.user}", icon_url = interaction.user.avatar)
