c8a1506b7ce341c5b8fb6f61cc119f510f34fb68
Walter Raja <waltermichelraja2@gmail.com>
2022-12-03 14:56:20 +0530

    updated

--- main.py
+++ main.py
@@ -26,9 +26,13 @@ async def on_ready():
 
 @client.tree.command(name="ping", description="sends client latency")
 async def ping(interaction:discord.Interaction):
-    uptime = str(datetime.timedelta(seconds=int(round(time.time()-startTime))))
-    embed = discord.Embed(title="", description=f"```yaml\nPing:   {client.latency*1000:,.0f} ms \nUptime: {uptime}```")
+    uptime=str(datetime.timedelta(seconds=int(round(time.time()-startTime))))
+    embed=discord.Embed(title="", description=f"```elm \nPing:   {client.latency*1000:,.0f} ms \nUptime: {uptime}```")
+    start=time.time()
     await interaction.response.send_message("**Pong!**", embed=embed, ephemeral=False)
+    end=time.time()
+    embed=discord.Embed(title="", description=f"```elm \nPing:         {client.latency*1000:,.0f} ms \nResponseTime: {(end-start)*1000:,.0f} ms \nUptime:       {uptime}```")
+    await interaction.edit_original_response(content="**Pong!**", embed=embed)
 
 
 client.run(TOKEN)
