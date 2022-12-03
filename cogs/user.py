c1b71f760c57bc89b140ae219c3f1d01bdc136a1
Walter Raja <waltermichelraja2@gmail.com>
2022-12-04 01:10:38 +0530

    updated

--- cogs/user.py
+++ cogs/user.py
@@ -6,9 +6,14 @@ class User(commands.Cog):
     def __init__(self, client):
         self.client=client
 
-    @app_commands.command(name="cog", description="cog test")
-    async def cog(self, interaction:discord.Interaction):
-        await interaction.response.send_message("hello!", ephemeral=True)
+    @app_commands.command(name="userinfo", description="sends user-details")
+    async def cog(self, interaction:discord.Interaction, user:discord.User=None):
+        user=interaction.message.author if not user else user
+        embed=discord.Embed(title="User Details", color = interaction.message.author.color, timestamp = interaction.message.created_at)
+        embed.add_field(name="Account creation: ", value=user.created_at.strftime("```%a, %#d %B %Y, %I:%M %p```"), inline=True)
+        embed.add_field(name="Joined at: ", value=user.joined_at.strftime("```%a, %#d %B %Y, %I:%M %p```"), inline=True)
+        embed.set_footer(text = f"Requested by {interaction.message.author}", icon_url = interaction.message.author.avatar_url)
+        await interaction.response.send_message(embed=embed, ephemeral=False)
 
 async def setup(client):
     await client.add_cog(User(client))
