2a078117348575751cc3850b2c9ed2dd0812c0c7
Walter Raja <waltermichelraja2@gmail.com>
2022-12-04 08:57:52 +0530

    updated

--- cogs/user.py
+++ cogs/user.py
@@ -9,11 +9,17 @@ class User(commands.Cog):
     @app_commands.command(name="userinfo", description="sends user-details")
     async def userinfo(self, interaction:discord.Interaction, user:discord.User=None):
         user=interaction.user if not user else user
-        embed=discord.Embed(title=f"User Details | {interaction.user}", color = interaction.user.color, timestamp = interaction.created_at)
-        embed.add_field(name="Account creation: ", value=user.created_at.strftime("```%a, %#d %B %Y, %I:%M %p```"), inline=True)
-        embed.add_field(name="Joined at: ", value=user.joined_at.strftime("```%a, %#d %B %Y, %I:%M %p```"), inline=True)
-        embed.set_footer(text = f"Requested by {interaction.user}", icon_url = interaction.user.avatar)
-        await interaction.response.send_message(embed=embed, ephemeral=False)
+        if interaction.guild.get_member(user) is not None:
+            embed=discord.Embed(title=f"User Details | {interaction.user}", color=interaction.user.color)
+            embed.add_field(name="Account creation: ", value=user.created_at.strftime("`%a, %#d %B %Y, %I:%M %p`"), inline=True)
+            embed.add_field(name="Joined at: ", value=user.joined_at.strftime("`%a, %#d %B %Y, %I:%M %p`"), inline=True)
+            embed.set_footer(text=f"ID: {interaction.user.id}")
+            await interaction.response.send_message(embed=embed, ephemeral=False)
+        else:
+            embed=discord.Embed(title=f"User Details | {interaction.user}", color=interaction.user.color)
+            embed.add_field(name="Account creation: ", value=user.created_at.strftime("`%a, %#d %B %Y, %I:%M %p`"), inline=True)
+            embed.set_footer(text=f"ID: {interaction.user.id}")
+            await interaction.response.send_message(embed=embed, ephemeral=False)
 
 async def setup(client):
     await client.add_cog(User(client))
