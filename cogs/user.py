3e741cba80b4c39d1ce4754620d1061e1392c643
Walter Raja <waltermichelraja2@gmail.com>
2022-12-04 19:40:00 +0530

    updated[2]

--- cogs/user.py
+++ cogs/user.py
@@ -8,18 +8,11 @@ class User(commands.Cog):
 
     @app_commands.command(name="userinfo", description="sends user-details")
     async def userinfo(self, interaction:discord.Interaction, user:discord.User=None):
-        if interaction.guild.get_member(user) is not None:
-            user=interaction.user if not user else user
-            embed=discord.Embed(title=f"User Details | {interaction.user}", color=interaction.user.color)
-            embed.add_field(name="Account creation: ", value=user.created_at.strftime("`%a, %#d %B %Y, %I:%M %p`"), inline=True)
-            embed.add_field(name="Joined at: ", value=interaction.user.joined_at.strftime("`%a, %#d %B %Y, %I:%M %p`"), inline=True)
-            embed.set_footer(text=f"ID: {interaction.user.id}")
-            await interaction.response.send_message(embed=embed, ephemeral=False)
-        else:
-            embed=discord.Embed(title=f"User Details | {interaction.user}", color=interaction.user.color)
-            embed.add_field(name="Account creation: ", value=user.created_at.strftime("`%a, %#d %B %Y, %I:%M %p`"), inline=True)
-            embed.set_footer(text=f"ID: {interaction.user.id}")
-            await interaction.response.send_message(embed=embed, ephemeral=False)
+        user=interaction.user if not user else user
+        embed=discord.Embed(title=f"`User Details | {interaction.user}`", color=interaction.user.color)
+        embed.add_field(name="Account creation: ", value=user.created_at.strftime("`%a, %#d %B %Y, %I:%M %p`"), inline=True)
+        embed.set_footer(text=f"ID: {interaction.user.id}")
+        await interaction.response.send_message(embed=embed, ephemeral=False)
 
 async def setup(client):
     await client.add_cog(User(client))
