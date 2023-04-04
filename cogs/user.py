325494c51732bbeb69d7fc32a58cce14712d9939
Walter Raja <waltermichelraja2@gmail.com>
2023-04-04 19:21:27 +0530

    updated

--- cogs/user.py
+++ cogs/user.py
@@ -9,18 +9,19 @@ from contextlib import redirect_stdout
 
 class Eval(commands.Cog):
     def __init__(self, client):
-        self.client = client
-        self.result = ""
+        self.client=client
+        self.result=""
 
     def escape_quote(self, content):
         if content.startswith("```") and content.endswith("```"):
             return "\n".join(content.split("\n")[1:-1])
         return content.strip("` \n")
-
+    
+    @app_commands.command(name="execute")
     @commands.command(aliases=["evaluate", "exe", "execute"], pass_context=True)
     @commands.is_owner()
     async def eval(self, ctx, *, body: str):
-        env = {
+        env={
             "discord": discord,
             "commands": commands,
             "client": self.client,
@@ -36,34 +37,32 @@ class Eval(commands.Cog):
             "time": time
         }
         env.update(globals())
-        body = self.escape_quote(body)
-        stdout = io.StringIO()
-        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+        body=self.escape_quote(body)
+        stdout=io.StringIO()
+        to_compile=f'async def func():\n{textwrap.indent(body, "  ")}'
         try:
             exec(to_compile, env)
         except Exception as e:
-            evem = discord.Embed(title = "", description = f"`Result`\n```py\n{e.__class__.__name__}: {e}\n```")
-            return await ctx.send(embed = evem)
-        func = env["func"]
+            evem=discord.Embed(title="", description=f"`Result`\n```py\n{e.__class__.__name__}: {e}\n```")
+            return await ctx.send(embed=evem)
+        func=env["func"]
         try:
             with redirect_stdout(stdout):
-                ret = await func()
+                ret=await func()
         except Exception as e:
-            value = stdout.getvalue()
-            evem = discord.Embed(title = "", description = f"`Result`\n```py\n{value}{traceback.format_exc()}\n```")
-            await ctx.send(embed = evem)
+            value=stdout.getvalue()
+            evem=discord.Embed(title="", description=f"`Result`\n```py\n{value}{traceback.format_exc()}\n```")
+            await ctx.send(embed=evem)
         else:
-            value = stdout.getvalue()
+            value=stdout.getvalue()
             if ret is None:
                 if value:
-                    evem = discord.Embed(title = "", description = f"`Result`\n```py\n{value}\n```")
-                    await ctx.send(embed = evem)
+                    evem=discord.Embed(title="", description=f"`Result`\n```py\n{value}\n```")
+                    await ctx.send(embed=evem)
             else:
-                self.result = ret
-                evem = discord.Embed(title = "", description = f"`Result`\n```py\n{value}{ret}\n```")
-                await ctx.send(embed = evem)
+                self.result=ret
+                evem=discord.Embed(title="", description=f"`Result`\n```py\n{value}{ret}\n```")
+                await ctx.send(embed=evem)
 
-def setup(client):
-    client.add_cog(Eval(client))
-
-
+async def setup(client):
+    await client.add_cog(Eval(client))
