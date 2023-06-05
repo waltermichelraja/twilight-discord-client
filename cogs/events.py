7881cab3293a6f49c599e778e67a5c7e968f4af7
Walter Raja <waltermichelraja2@gmail.com>
2023-06-06 00:34:34 +0530

    updated

--- cogs/events.py
+++ cogs/events.py
@@ -7,10 +7,11 @@ class Events(commands.Cog):
 
     @tasks.loop(minutes=15, reconnect=True)
     async def stat(self, ctx):
+        users=ctx.guild.members
         for guild in self.client.guilds:
           for channel in guild.channels:
             if channel.id==1047931306627039313:
-              await channel.edit(name=f"Members: {len(list(filter(lambda m: not m.bot, ctx.guild.members)))}")
+              await channel.edit(name=f"Members: {len(list(filter(lambda m: not m.bot, users)))}")
     @stat.before_loop
     async def stat_before(self):
         await self.client.wait_until_ready()
